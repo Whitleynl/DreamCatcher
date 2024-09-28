459e4c202689c464e3f919eb364b17235ce75f8d
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2024-09-28 13:34:26 -0400

    frontend init

---
+++ dreamcatcher-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="p-8 bg-blue-500 text-white">
+      <h1 className="text-3xl font-bold">Dreamcatcher App</h1>
+      <p className="mt-4">TailwindCSS is working!</p>
+    </div>
+  );
+}
+
+export default App;
