e91fe7cf27396dd4bdd2d23b15a40b18da038e7a
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2024-09-28 21:10:08 -0400

    Configured PostgreSQL for Heroku

--- dreamcatcher/settings.py
+++ dreamcatcher/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,12 +81,17 @@ WSGI_APPLICATION = "dreamcatcher.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
+# Enable SSL for Heroku PostgreSQL
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'].update({
+        'CONN_MAX_AGE': 500,
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
+    }) 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
