97859f8b75443fbf8b3ba0132917487a05729495
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2024-09-30 09:42:11 -0400

    fir api path structure

--- dream_entries/urls.py
+++ dream_entries/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import DreamListCreateView, DreamSearchView, DreamDeleteView
 
 urlpatterns = [
-    path('dreams/', DreamListCreateView.as_view(), name='dream-list-create'),
-    path('dreams/search/', DreamSearchView.as_view(), name='dream-search'),
-    path('dreams/<int:id>/', DreamDeleteView.as_view(), name='dream-delete'),
+    path('', DreamListCreateView.as_view(), name='dream-list-create'),
+    path('search/', DreamSearchView.as_view(), name='dream-search'),  
+    path('<int:id>/', DreamDeleteView.as_view(), name='dream-delete'),
 ]
