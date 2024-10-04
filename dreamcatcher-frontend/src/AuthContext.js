c9b56975b781f4bffa7a4f26a35985da1b03430d
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2024-10-04 13:49:10 -0400

    add authcontext

---
+++ dreamcatcher-frontend/src/AuthContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState, useEffect } from "react";
+import api from "./services/api";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [authToken, setAuthToken] = useState(localStorage.getItem("authToken")) || null;
+
+    useEffect(() => {
+        if (authToken) {
+            localStorage.setItem("authToken", authToken);
+            api.defaults.headers.common["Authorization"] = `Token ${authToken}`;
+        } else {
+            localStorage.removeItem("authToken");
+            delete api.defaults.headers.common["Authorization"];
+        }
+    }, [authToken]);
+
+    return (
+        <AuthContext.Provider value={{ authToken, setAuthToken }}>
+            {children}
+        </AuthContext.Provider> 
+    );
+};
