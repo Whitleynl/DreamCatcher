4a4337e673dc6059c8fe22ab6003ab3a268b84c7
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2024-10-28 14:30:49 -0400

    user dream filtering

--- dream_entries/views.py
+++ dream_entries/views.py
@@ -8,18 +8,28 @@ class DreamListCreateView(generics.ListCreateAPIView):
     serializer_class = DreamSerializer
     
     def get_queryset(self):
-        return Dream.objects.all()  # For now, just get all dreams
+        # Only return dreams for the current user
+        return Dream.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        # Automatically set the user when creating a new dream
+        serializer.save(user=self.request.user)
 
 class DreamSearchView(generics.ListAPIView):
     serializer_class = DreamSerializer
     
     def get_queryset(self):
         search = self.request.query_params.get('q', '')
+        # Only search through the current user's dreams
         return Dream.objects.filter(
-            Q(title__icontains=search) | Q(description__icontains=search)
+            Q(user=self.request.user) & 
+            (Q(title__icontains=search) | Q(description__icontains=search))
         )
 
 class DreamDeleteView(generics.RetrieveDestroyAPIView):
     serializer_class = DreamSerializer
     lookup_field = 'id'
-    queryset = Dream.objects.all()
+    
+    def get_queryset(self):
+        # Only allow deleting the current user's dreams
+        return Dream.objects.filter(user=self.request.user)
