a4c07231fe3314c32786f9129dee695fa374e386
Nathan Whitley <112634487+Whitleynl@users.noreply.github.com>
2024-10-28 14:07:53 -0400

    fix data privacy

--- dream_entries/views.py
+++ dream_entries/views.py
@@ -5,17 +5,24 @@ from .serializers import DreamSerializer
 from django.db.models import Q
 
 class DreamListCreateView(generics.ListCreateAPIView):
-    queryset = Dream.objects.all()
     serializer_class = DreamSerializer
+    
+    def get_queryset(self):
+        return Dream.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
     
 class DreamSearchView(generics.ListAPIView):
     serializer_class = DreamSerializer
     def get_queryset(self):
         search = self.request.query_params.get('q', '')
-        return Dream.objects.filter(Q(title__icontains=search) | Q(description__icontains=search))
+        return Dream.objects.filter(Q(user=self.request.user) & (Q(title__icontains=search) | Q(description__icontains=search)))
     
 class DreamDeleteView(generics.RetrieveDestroyAPIView):
-    queryset = Dream.objects.all()
     serializer_class = DreamSerializer
     lookup_field = 'id'
+    
+    def get_queryset(self):
+        return Dream.objects.filter(user=self.request.user)
 
